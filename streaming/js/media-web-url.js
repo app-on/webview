edc736c9031fcb3a34eef88b0a0130cbfd6c33a4
app-on <132430427+app-on@users.noreply.github.com>
2025-07-31 23:56:17 -0500

    Add files via upload

--- streaming/js/media-web-url.js
+++ streaming/js/media-web-url.js
@@ -63,7 +63,9 @@ class MediaWebUrl {
   };
   static streamwish = ({ url }) => {
     return new Promise((resolve) => {
-      this.fetch(url)
+      const urlModify = `https://habetar.com/e/${url.split("/").pop()}`;
+
+      this.fetch(urlModify)
         .then((text) => {
           const $text = document.createElement("div");
           $text.innerHTML = text;
